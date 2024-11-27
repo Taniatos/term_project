e71fba366398b2a47b7ce8783ddb7eac2b1b1eda
Tetiana Korchynska <tanakorchynska@gmail.com>
2024-11-27 20:46:28 +0200

    Update Traveling.js

--- my-app/src/components/HobbieOne/Traveling.js
+++ my-app/src/components/HobbieOne/Traveling.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Navigation from "../Navigation/Navigation";
+import "./Traveling.css";
 
 function Traveling() {
   return (
     <div>
+      <Navigation />
       <h1>Traveling Page</h1>
       <p>traveling.</p>
     </div>
